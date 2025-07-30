11da10b21a739a88703038b92956e47501293c08
Dharmik Parakhiya <parakhiyadharmik09@gmail.com>
2025-07-30 08:09:52 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+             
+            <p>Hey There, This is a TodosList , where you can add and delete your list for daily work...</p>
+        </div>
+    )
+}
